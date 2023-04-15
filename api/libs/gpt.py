487a3d61d67c86eb2e714703bf85915d6636aeb3
Jonathan Tavares <jonathantavares.all@gmail.com>
2023-04-15 13:54:33 -0300

    fix: change temperature to improve GPT response

--- api/libs/gpt.py
+++ api/libs/gpt.py
@@ -3,11 +3,11 @@ import api.libs.utils as utils
 
 def summarize(api_key:str, action:str, text:str, content_type:str, language:str="pt-BR"):
     openai.api_key = api_key
-
+    prompt = utils.generate_prompt(action, text, content_type, language)
     response = openai.Completion.create(
         engine='text-davinci-003',
-        prompt=utils.generate_prompt(action, text, content_type, language),
-        temperature=0.1,
+        prompt=prompt,
+        temperature=1,
         max_tokens=2048,
         n=1,
         stop=None
